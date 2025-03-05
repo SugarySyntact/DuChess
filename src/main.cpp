f96abb1206244f9c9340ac0bc10daa6e2ec41784
SugarySyntact <sugarysyntact@gmail.com>
2025-03-05 15:46:23 -0700

    build: update cmake to correctly link main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 
+#include "bitboard.h"
+
+using namespace Chess;
+using namespace Util;
+
 auto main() -> int
 {
+    Bitboards::init();
+
     std::cout << "\n╔══════════════════════════════════════╗";
     std::cout << "\n║            DuChess Engine            ║";
     std::cout << "\n╚══════════════════════════════════════╝\n";
+
+    std::cout << "\nKnight's moves from E4\n";
+    Bitboard knight_moves = 0;
+
+    knight_moves |= northOne(northEastOne(square(Square::E4))); // NNE
+    knight_moves |= eastOne(northEastOne(square(Square::E4)));  // ENE
+    knight_moves |= eastOne(southEastOne(square(Square::E4)));  // ESE
+    knight_moves |= southOne(southEastOne(square(Square::E4))); // SSE
+    knight_moves |= southOne(southWestOne(square(Square::E4))); // SSW
+    knight_moves |= westOne(southWestOne(square(Square::E4)));  // WSW
+    knight_moves |= westOne(northWestOne(square(Square::E4)));  // WNW
+    knight_moves |= northOne(northWestOne(square(Square::E4))); // NNW
+
+    Bitboards::print(knight_moves);
 
     return 0;
 }
