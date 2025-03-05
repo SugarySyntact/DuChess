9595ec2d98458aa5b039214bbcad933c9103bfd5
SugarySyntact <sugarysyntact@gmail.com>
2025-03-04 17:42:38 -0700

    chore: init

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+auto main() -> int
+{
+    std::cout << "\n╔══════════════════════════════════════╗";
+    std::cout << "\n║            DuChess Engine            ║";
+    std::cout << "\n╚══════════════════════════════════════╝\n";
+
+    return 0;
+}
