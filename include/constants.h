0fd1bac249c37e0b651d9edfaff59a510b163e78
SugarySyntact <sugarysyntact@gmail.com>
2025-03-04 19:43:21 -0700

    feat(utils): implement basic types
    - define enumerations (Square, PieceType, Color, Piece)
    - create utility functions for type conversions

---
+++ include/constants.h
@@ -0,0 +1,14 @@
+#ifndef CHESS_CONSTANTS_H
+#define CHESS_CONSTANTS_H
+
+namespace Chess::Constants {
+
+constexpr int PIECE_COLOR_OFFSET = 8;
+
+constexpr int BOARD_SIZE = 8;
+
+constexpr int NO_SQUARE = -1;
+
+} // namespace Chess::Constants
+
+#endif // CHESS_CONSTANTS_H
---
+++ src/types.cpp
@@ -0,0 +1,57 @@
+#include "types.h"
+
+#include <stdexcept>
+
+#include "constants.h"
+
+namespace Chess::Util {
+
+auto getPieceColor(Piece piece) -> Color
+{
+    if (piece == Piece::NONE) { return Color::NONE; }
+
+    int color_value = static_cast<int>(piece) / Constants::PIECE_COLOR_OFFSET;
+    return static_cast<Color>(color_value);
+}
+
+auto getPieceType(Piece piece) -> PieceType
+{
+    if (piece == Piece::NONE) { return PieceType::NONE; }
+
+    int piece_value = static_cast<int>(piece) % Constants::PIECE_COLOR_OFFSET;
+    return static_cast<PieceType>(piece_value);
+}
+
+auto makePiece(PieceType type, Color color) -> Piece
+{
+    if (type == PieceType::NONE) { return Piece::NONE; }
+    if (color == Color::NONE) { return Piece::NONE; }
+
+    int piece_value = static_cast<int>(type);
+    if (color == Color::BLACK) { piece_value += Constants::PIECE_COLOR_OFFSET; }
+
+    return static_cast<Piece>(piece_value);
+}
+
+auto getFile(Square square) -> int
+{
+    if (square == Square::NONE) { return Constants::NO_SQUARE; }
+    return static_cast<int>(square) % Constants::BOARD_SIZE;
+}
+
+auto getRank(Square square) -> int
+{
+    if (square == Square::NONE) { return Constants::NO_SQUARE; }
+    return static_cast<int>(square) / Constants::BOARD_SIZE;
+}
+
+auto makeSquare(int file, int rank) -> Square
+{
+    if (file < 0 || file > Constants::BOARD_SIZE - 1 || rank < 0 ||
+        rank > Constants::BOARD_SIZE - 1) {
+        return Square::NONE;
+    }
+    return static_cast<Square>((rank * Constants::BOARD_SIZE) + file);
+}
+
+} // namespace Chess::Util
